193519b56a34dd4802f544e59ad1c55efca69744
Boyang Chen <boaryang17@gmail.com>
2025-04-08 17:37:02 -0400

    Added GetStartedView
    Also updated the welcome page

---
+++ PCPal/GetStartedView.swift
@@ -0,0 +1,18 @@
+//
+//  GetStartedView.swift
+//  PCPal
+//
+//  Created by Boyang Chen on 4/8/25.
+//
+
+import SwiftUI
+
+struct GetStartedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GetStartedView()
+}
--- PCPal/WelcomeScreenView.swift
+++ PCPal/WelcomeScreenView.swift
@@ -10,10 +10,38 @@ import SwiftUI
 struct WelcomeScreenView: View {
     var body: some View {
         NavigationView {
-            List {
-                NavigationLink(destination: Text("View 1")) {
-                                    Text("Get Started")
-                                }
+            VStack(alignment: .center, spacing: 10) {
+                Text("Home")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.horizontal)
+                
+                List {
+                    NavigationLink(destination: GetStartedView()) {
+                        Image(systemName: "play.fill")
+                        Text("Get Started")
+                            .font(.title3)
+                            .padding(.vertical, 12)
+                    }
+                    NavigationLink(destination: ComputerPartsView()) {
+                        Image(systemName: "cpu")
+                        Text("Computer Parts")
+                            .font(.title3)
+                            .padding(.vertical, 12)
+                    }
+                    NavigationLink(destination: BuildResourcesView()) {
+                        Image(systemName: "richtext.page")
+                        Text("Useful Build Resources")
+                            .font(.title3)
+                            .padding(.vertical, 12)
+                    }
+                    NavigationLink(destination: OurRecommendationView()) {
+                        Image(systemName: "hand.thumbsup.fill")
+                        Text("Our Recommendations")
+                            .font(.title3)
+                            .padding(.vertical, 12)
+                    }
+                }
             }
         }
     }
