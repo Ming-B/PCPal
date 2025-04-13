b6cf076f5e4f53a0d756662249cef90bf51121eb
Ming Bian <127263871+Ming-B@users.noreply.github.com>
2025-04-12 22:10:33 -0400

    temp UI screen to demo Build Recs

--- PCPal/OurRecommendationView.swift
+++ PCPal/OurRecommendationView.swift
@@ -8,9 +8,98 @@
 import SwiftUI
 
 struct OurRecommendationView: View {
+    @State private var showDetails = false;
+    @State private var chevronRotate = 0.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        Text("Our PC Builds")
+            .font(.largeTitle)
+            .foregroundStyle(.blue)
+            .padding()
+        
+        Spacer()
+        
+   
+            HStack {
+                Button("", systemImage: "chevron.right") {
+                    withAnimation(.linear) {
+                        showDetails.toggle()
+                        if showDetails {
+                            chevronRotate += 90.0
+                        }
+                        else {
+                            chevronRotate -= 90.0
+                        }
+                    }
+                }
+                .rotationEffect(.degrees(chevronRotate))
+                Section(header: Text("Budget Tier").font(.headline)) {
+                    
+                    if showDetails {
+                        Text("CPU: Intel Core i5-10300H\nGPU: NVIDIA GeForce GTX 1650 Super\nRAM: 8GB DDR4\nStorage: 512GB SSD\nOperating System: Windows 10 Home")
+                    }
+                    
+                    
+                }
+            }
+            .padding()
+        
+            Spacer()
+            
+        HStack {
+            Button("", systemImage: "chevron.right") {
+                withAnimation(.linear) {
+                    showDetails.toggle()
+                    if showDetails {
+                        chevronRotate += 90.0
+                    }
+                    else {
+                        chevronRotate -= 90.0
+                    }
+                }
+            }
+            .rotationEffect(.degrees(chevronRotate))
+            Section(header: Text("Mid-Tier").font(.headline)) {
+                
+                if showDetails {
+                    Text("CPU: Intel Core i5-10300H\nGPU: NVIDIA GeForce GTX 1650 Super\nRAM: 8GB DDR4\nStorage: 512GB SSD\nOperating System: Windows 10 Home")
+                }
+                
+                
+            }
+        }
+        .padding()
+            
+            Spacer()
+            
+        HStack {
+            Button("", systemImage: "chevron.right") {
+                withAnimation(.linear) {
+                    showDetails.toggle()
+                    if showDetails {
+                        chevronRotate += 90.0
+                    }
+                    else {
+                        chevronRotate -= 90.0
+                    }
+                }
+            }
+            .rotationEffect(.degrees(chevronRotate))
+            Section(header: Text("High End").font(.headline)) {
+                
+                if showDetails {
+                    Text("CPU: Intel Core i5-10300H\nGPU: NVIDIA GeForce GTX 1650 Super\nRAM: 8GB DDR4\nStorage: 512GB SSD\nOperating System: Windows 10 Home")
+                }
+                
+                
+            }
+        }
+        .padding()
+            Spacer()
+        }
+        
+        
+    
 }
 
 #Preview {
