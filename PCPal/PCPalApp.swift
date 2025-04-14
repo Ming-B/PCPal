3ac765b32e2f6bc83f498ed7e75aeba718979b12
LuckyDuck307 <165798454+LuckyDuck307@users.noreply.github.com>
2025-04-14 12:44:52 -0400

    Update PCPalApp.swift with model context

--- PCPal/PCPalApp.swift
+++ PCPal/PCPalApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct PCPalApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CartView()
         }
+        .modelContainer(for: PCCart.self)
     }
 }
